0d68caa714b317adbf133bf1b6a9cb8276b9ed43
Alfie England <16434320@eng.nene.ac.uk>
2017-02-26 22:33:04 +0000

    Added customer_payments table create

--- create_22.sql
+++ create_22.sql
@@ -1,1 +1,9 @@
 -- CSY2026 Group 22 Assignment Creates
+CREATE TABLE customer_payments(
+customer_payment_id     NUMBER(10),
+customer_id             NUMBER(10),
+payment_id              NUMBER(10),
+amount_due              NUMBER(5),
+payment_received        CHAR,
+transaction_date        DATE
+);
