0c02f683d23ccb3c0e9babc7a8b362428d8d563f
unknown <16434320@NW041-15044.eng.nene.ac.uk>
2017-04-24 21:35:52 +0100

    Added hotels, customers and locations queries

--- queries_22.sql
+++ queries_22.sql
@@ -1,1 +1,41 @@
 -- CSY2026 Group 22 Assignment Queries
+SELECT *
+FROM hotels;
+
+SELECT DISTINCT gender
+FROM customers;
+
+SELECT DISTINCT c_country
+FROM customers;
+
+SELECT title, customer_fname, customer_sname, contact_number, email
+FROM customers
+WHERE customer_id = 0000000001;
+
+SELECT title, customer_fname, customer_sname, contact_number, email
+FROM customers
+WHERE customer_id = 0000000002;
+
+SELECT title, customer_fname, customer_sname, contact_number, email
+FROM customers
+WHERE customer_id = 0000000003;
+
+SELECT title, customer_fname, customer_sname, contact_number, email
+FROM customers
+WHERE customer_id = 0000000004;
+
+SELECT title, customer_fname, customer_sname, contact_number, email
+FROM customers
+WHERE customer_id = 0000000005;
+
+SELECT customer_id, title, customer_fname, customer_sname
+FROM customers
+WHERE town = 'NORTHAMPTON';
+
+SELECT customer_id, title, customer_fname, customer_sname
+FROM customers
+WHERE town NOT LIKE 'NORTHAMPTON';
+
+SELECT *
+FROM locations
+WHERE area LIKE 'U%';
